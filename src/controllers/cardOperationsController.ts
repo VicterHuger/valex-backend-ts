062a45cc82ecae6bd98d7d2494c81ab92cea6393
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-09-02 12:55:24 -0300

    chore: create card expiration date

--- src/controllers/cardOperationsController.ts
+++ src/controllers/cardOperationsController.ts
@@ -1,13 +1,14 @@
 import { Request, Response} from "express";
-import { Card } from "../repositories/cardRepository";
+import { TransactionTypes } from "../repositories/cardRepository";
 
 import * as cardOperationsService from "../services/cardOperationServices"
 
 export async function createCard(req:Request, res:Response){
-    const body:Card= res.locals.body;
+    const employeeId:number = res.locals.id;
+    const body:{type:TransactionTypes}= res.locals.body;
     const apiKey:string = res.locals.apiKey;
 
-    await cardOperationsService.createCard(body,apiKey);
+    await cardOperationsService.createCard(body,employeeId,apiKey);
 
     return res.sendStatus(201);
 }
--- src/services/cardOperationServices.ts
+++ src/services/cardOperationServices.ts
@@ -1,71 +1,65 @@
+import dayjs from 'dayjs';
+import { faker } from '@faker-js/faker';
 import * as cardRepository from '../repositories/cardRepository'
 import * as companyRepository from '../repositories/companyRepository'
 import * as employeeRepository from '../repositories/employeeRepository';
-import { Card } from '../repositories/cardRepository';
 import { generateThrowErrorMessages } from '../middlewares/errorHandlerMiddleware';
-import { faker } from '@faker-js/faker';
 
 
 
-export async function createCard(body:Card,headers:string){
-    // const company:companyRepository.Company |undefined = await companyRepository.findByApiKey(headers);
-    
+export async function createCard(body: { type: cardRepository.TransactionTypes }, employeeId: number, headers: string) {
+
     await verifyReturnExistingItem(headers, companyRepository.findByApiKey, "Unauthorized", "There is no company with this API-key");
-    
-    const employee = await verifyReturnExistingItem(body.employeeId, employeeRepository.findById, "NotFound", "There is no employee with this id");
 
-    const cardEmployee:cardRepository.Card | undefined = await cardRepository.findByTypeAndEmployeeId(body.type,body.employeeId);
+    const employee:employeeRepository.Employee = await verifyReturnExistingItem(employeeId, employeeRepository.findById, "NotFound", "There is no employee with this id");
 
-    if(cardEmployee){
+    const card:cardRepository.CardUpdateData= {
+        employeeId,
+        type: body.type, 
+    }
+
+    const cardEmployee: cardRepository.Card | undefined = await cardRepository.findByTypeAndEmployeeId(body.type, employeeId);
+
+    if (cardEmployee) {
         generateThrowErrorMessages("Conflict", "This employee has already an card of this type!")
     }
 
-    const cardNumber = faker.random.numeric(16, {allowLeadingZeros:true});
+    card.number = createCardNumber() ;
 
-    const cardName:string = createCardName(employee.fullName);
-    console.log(cardName);
-    
-    
-    // if(company===undefined){
-    //     const errorApi:errorObject= {
-    //         code: "Unauthorized",
-    //         message: "There is no company with this API-key"
-    //     } 
-    //     throw(errorApi);
-    // } 
+    card.cardholderName = createCardName(employee.fullName);
 
-    // const employee: employeeRepository.Employee | undefined = await employeeRepository.findById(body.employeeId);
-
-    // console.log(employee);
-
-    // if(employee===undefined){
-    //     const errorEmployeeId = {
-    //         code: "NotFound",
-    //         message: `There is no employee with this id`
-    //     }
-    //     throw(errorEmployeeId);
-    // }
+    card.expirationDate = createCardExpirationDate(); 
 
 }
 
-async function verifyReturnExistingItem(item:string|number, callback:any, code:string, message:string){
+async function verifyReturnExistingItem(item: string | number, callback: any, code: string, message: string) {
+    
     const itemSearched = await callback(item);
-    if(itemSearched===undefined){
-        generateThrowErrorMessages(code,message);
+    
+    if (itemSearched === undefined) {
+        generateThrowErrorMessages(code, message);
     }
     return itemSearched;
 }
 
-function createCardName(string:string){
-    const newString:string= string.toUpperCase();
-    const names: string[]=newString.split(" ");
-    const arrayNewNames=[names[0]];
+function createCardName(string: string) {
+    const newString: string = string.toUpperCase();
+    const names: string[] = newString.split(" ");
+    const arrayNewNames = [names[0]];
 
-    for (let i:number=1; i<names.length -1; i++){
-        if(names[i].length>=3) arrayNewNames.push(names[i][0]);
+    for (let i: number = 1; i < names.length - 1; i++) {
+        if (names[i].length >= 3) arrayNewNames.push(names[i][0]);
     }
-    
-    arrayNewNames.push(names[names.length-1]);
-  
+
+    arrayNewNames.push(names[names.length - 1]);
+
     return arrayNewNames.join(" ");
+}
+
+function createCardNumber(){
+    return faker.random.numeric(16, { allowLeadingZeros: true })
+}
+
+function createCardExpirationDate(){
+    return dayjs().add(5,"y").format("MM/YY");
 }
